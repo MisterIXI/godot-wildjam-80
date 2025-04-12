8f9345aadc5268221c97353eafb59c78f471cada
SteffWin <steffen.winternheimer@gmail.com>
2025-04-12 16:32:15 +0200

    UPDATE score system to check on y axis

--- scenes/_debug/Steffen/scoreText.gd
+++ scenes/_debug/Steffen/scoreText.gd
@@ -1,15 +1,11 @@
 extends Label
 
-var scoreText = "WOOP WOOP"
 @export var playerNode : RigidBody2D 
-var startX : int
+var startY : int
 
 func _ready() -> void:
-	text = scoreText
-	startX = playerNode.global_position.x
+	startY = playerNode.global_position.y
 	
 func _process(_delta: float) -> void:
-	var currentPos : int = (playerNode.global_position.x - startX) / 10
-	if(currentPos <= 0):
-		currentPos = 0
+	var currentPos : int = (-playerNode.global_position.y + startY) / 10
 	text = str(currentPos)
