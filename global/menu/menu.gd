0eca74ceade04a618d765df28d6548f0c45d6c66
OliPohl <oliver@pohl66.de>
2025-04-12 19:04:05 +0200

    settings menu

---
+++ global/menu/menu.gd
@@ -0,0 +1,6 @@
+extends Control
+class_name Menu
+
+@export var main_menu : MainMenu
+@export var pause_menu : PauseMenu
+@export var settings_menu : SettingsMenu
---
+++ global/menu/settings_menu/settings_menu.gd
@@ -0,0 +1,8 @@
+extends Control
+class_name SettingsMenu
+
+@export var confirm_button : Button
+@export var master_volume_slider : HSlider
+@export var music_volume_slider : HSlider
+@export var sfx_volume_slider : HSlider
+@export var ambiences_volume_slider : HSlider
