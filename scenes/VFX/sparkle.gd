94d3e4f364e4ac5bd2c9c67411136cceea27764c
Yannik Braendle <yannik.braendle@t-online.de>
2025-04-13 18:15:23 +0200

    added sparkles for rope and fixed them

--- scenes/VFX/sparkle.gd
+++ scenes/VFX/sparkle.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+class_name SparkleEmitter
 
 @onready var cpu_particles_2d: CPUParticles2D = %CPUParticles2D
 @onready var cpu_particles_2d_2: CPUParticles2D = %CPUParticles2D2
@@ -11,8 +11,8 @@ func _ready() -> void:
 	cpu_particles_2d_2.one_shot = true
 	cpu_particles_2d_3.one_shot = true
 
-func emit():
-	position = get_parent().global_position
+func emit_at(target_pos: Vector2):
+	global_position = target_pos
 	cpu_particles_2d.emitting = true
 	cpu_particles_2d_2.emitting = true
 	cpu_particles_2d_3.emitting = true
