2c6eec1cec7a8f3c20855c4a236c0e9601bc9f4f
OliPohl <oliver@pohl66.de>
2025-04-14 20:46:41 +0200

    fixed yanniks suggestion

--- global/grace/grace.gd
+++ global/grace/grace.gd
@@ -17,12 +17,12 @@ var timer: float = 0
 signal scene_loaded
 
 func _ready():
-  if reset_audio_bus_on_start and Engine.is_editor_hint():
+  if reset_audio_bus_on_start and not OS.get_name() == "Web":
     reset_audio_bus()
   else:
     load_audio_bus()
 
-  if reset_scene_on_start and Engine.is_editor_hint():
+  if reset_scene_on_start and not OS.get_name() == "Web":
     reset_scene()
   else:
     load_scene()
@@ -70,8 +70,7 @@ func reset_audio_bus():
 
 #region Scene
 func load_scene():
-# TODO: Add real engine check here
-  if ignore_saved_scene: # and Engine.is_editor_hint():
+  if ignore_saved_scene and not OS.get_name() == "Web":
     return
 
   var config = ConfigFile.new()
