23c3ad86e43defdd8f9625f563b520fcc02b92fd
OliPohl <oliver@pohl66.de>
2025-04-17 02:25:49 +0200

    very very radical loading time improvement

--- scenes/end_scene/respawn_area.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@export var _player : RigidBody2D
-var _startpos : Vector2  = Vector2.ZERO
-func _ready() -> void:
-    _startpos =  _player.global_position
-
-func on_body_entered(_body : Node2D) -> void:
-   _body.global_position = _startpos
