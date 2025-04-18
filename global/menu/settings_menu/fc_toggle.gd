67476d6ebdceedd32dd1bbfa5ed424e51b7872ac
Yannik Braendle <yannik.braendle@t-online.de>
2025-04-19 01:09:39 +0200

    fixed fullscreen button (reads current state on visible)

--- global/menu/settings_menu/fc_toggle.gd
+++ global/menu/settings_menu/fc_toggle.gd
@@ -2,8 +2,16 @@ extends CheckBox
 
 func _ready() -> void:
 	toggled.connect(_on_fc_show_toggled)
+	visibility_changed.connect(_on_fc_visibility_changed)
 
 func _on_fc_show_toggled(_bool : bool) -> void:
 	print("FC visible changed to ", _bool)
 	
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if _bool else DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_fc_visibility_changed():
+	# check for fullscreen
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		button_pressed = true
+	else:
+		button_pressed = false
