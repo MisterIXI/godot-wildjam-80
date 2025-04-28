199623e4063be7fb2781e66bad02d53a424c8468
Yannik Braendle <yannik.braendle@t-online.de>
2025-04-29 00:07:40 +0200

    target indicator permament with grayout and smooth snapping

--- scenes/Player/target_indicator.gd
+++ scenes/Player/target_indicator.gd
@@ -1,5 +1,8 @@
 extends Sprite2D
-
+@export var line_color_active: Color = Color("ff33a3")
+@export var line_color_inactive: Color = Color("51002f")
+@export var color_tint_active: Color = Color(1.0, 1.0, 1.0, 1.0)
+@export var color_tint_inactive: Color = Color(0.5, 0.5, 0.5, 1.0)
 
 @onready var parent: PlayerController = get_parent() as PlayerController
 
@@ -27,13 +30,25 @@ func _process(_delta):
 	# 	global_position = parent.global_position + mouse_dir * parent.max_rope_distance / 2
 	# 	rotation = mouse_dir.angle()
 	# 	scale = Vector2(parent.max_rope_distance, 4)
+	
+	var target_move
 	if result:
-		global_position = result.position
-		modulate = Color(0.0, 1.0, 0.0, 0.5)
+		target_move = result.position - global_position
+		# modulate = Color(0.0, 1.0, 0.0, 0.5)
+		material.set("shader_parameter/line_color", line_color_active);
+		material.set("shader_parameter/color_tint", color_tint_active);
 		visible = true
 		pass
 	else:
-		# global_position = parent.global_position + mouse_dir * parent.max_rope_distance
+
+		var mouse_dist = (mouse_pos - parent.global_position).length()
+		var target_pos = parent.global_position + mouse_dir * clampf(parent.max_rope_distance, 0.0, mouse_dist)
+		target_move = target_pos - global_position
 		# modulate = Color(1.0, 0.0, 0.0, 0.5)
-		visible = false
+		material.set("shader_parameter/line_color", line_color_inactive);
+		material.set("shader_parameter/color_tint", color_tint_inactive);
+		# visible = false
 		pass
+	if target_move.length() > 1:
+		target_move *= 0.7
+	global_position += target_move
